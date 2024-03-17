dd4604590ade3fb57bd97b42a22f5e71c3438e46
IamQhuynekk <157953172+SimmyDepvai@users.noreply.github.com>
2024-03-17 14:20:12 +0700

    Add files via upload

---
+++ HuyVietSub.lua
@@ -0,0 +1,3 @@
+local cboard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
+cboard("https://discord.gg/APDF6KQ56H")
+game.Players.LocalPlayer:Kick("Old script no longer works, join the Discord for the updated script discord.gg/APDF6KQ56H")
